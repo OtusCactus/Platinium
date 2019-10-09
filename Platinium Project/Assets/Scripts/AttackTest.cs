5bb1f4f96977c2c40cc813b8d1800843b771f19b
Grégoire <gpierillas@gmail.com>
2019-10-09 10:49:58 +0200

    Dossiers

---
+++ Platinium Project/Assets/Scripts/AttackTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackTest : MonoBehaviour
+{
+
+    public Transform uppercutPosition;
+    public float uppercutRadius;
+
+    public LayerMask EnemyMask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Collider2D[] enemiesCollider = Physics2D.OverlapCircleAll(uppercutPosition.position, uppercutRadius, EnemyMask);
+            for (int i = 0; i < enemiesCollider.Length; i++)
+            {
+                Debug.Log("Hit");
+            }
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(uppercutPosition.position, uppercutRadius);
+    }
+}
