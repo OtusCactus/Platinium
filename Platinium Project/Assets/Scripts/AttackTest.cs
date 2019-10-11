4ec1ad77e1a97bbb9137f31d7c559f61e2a252c8
Grégoire <gpierillas@gmail.com>
2019-10-11 10:12:27 +0200

    Shockwave modifiée

--- Platinium Project/Assets/Scripts/AttackTest.cs
+++ Platinium Project/Assets/Scripts/AttackTest.cs
@@ -6,38 +6,81 @@ public class AttackTest : MonoBehaviour
 {
 
     public Transform shockWavePosition;
+
     public float shockWaveRadius;
+    private float shockWaveCooldown;
+    public float shockWaveCooldownMax;
+
+    public GameObject shockWaveSprite;
+
+    public float shockWaveDurationMax;
+    private float shockWaveDuration;
+
+    private bool _isShockWaveButtonPressed;
+    private bool _isShockWavePossible;
 
     public LayerMask EnemyMask;
     public float _pushbackIntensity;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        shockWaveCooldown = 0;
+        shockWaveDuration = shockWaveDurationMax;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetKeyUp(KeyCode.A))
+        if (!_isShockWavePossible)
         {
-
-            Collider2D[] enemiesCollider = Physics2D.OverlapCircleAll(shockWavePosition.position, shockWaveRadius, EnemyMask);
-            if(enemiesCollider.Length > 0)
+            shockWaveSprite.SetActive(false);
+            shockWaveCooldown -= Time.deltaTime;
+            
+            if (shockWaveCooldown <= 0 )
             {
-                
-                for (int i = 0; i < enemiesCollider.Length; i++)
+                _isShockWavePossible = true;
+                shockWaveCooldown = shockWaveCooldownMax;
+            }
+            
+        }
+
+        if (Input.GetKeyDown("joystick button 0") && _isShockWavePossible)
+        {
+            _isShockWaveButtonPressed = true;
+        }
+
+        if (_isShockWaveButtonPressed && _isShockWavePossible)
+        {
+            shockWaveDuration -= Time.deltaTime;
+            if(shockWaveDuration > 0)
+            {
+                shockWaveSprite.SetActive(true);
+                Collider2D[] enemiesCollider = Physics2D.OverlapCircleAll(shockWavePosition.position, shockWaveRadius, EnemyMask);
+                if(enemiesCollider.Length > 0)
                 {
-                    Vector3 moveDirection = enemiesCollider[i].transform.position - this.transform.position;
-                    //enemiesCollider[i].GetComponent<Rigidbody2D>().velocity = this.transform.forward * Time.deltaTime * _pushbackIntensity;
-                    enemiesCollider[i].GetComponent<Rigidbody2D>().AddForce(moveDirection.normalized * Time.deltaTime * _pushbackIntensity);
-                    Debug.Log("Hit");
+                
+                    for (int i = 0; i < enemiesCollider.Length; i++)
+                    {
+                        Vector3 moveDirection = enemiesCollider[i].transform.position - this.transform.position;
+                        //enemiesCollider[i].GetComponent<Rigidbody2D>().velocity = this.transform.forward * Time.deltaTime * _pushbackIntensity;
+                        enemiesCollider[i].GetComponent<Rigidbody2D>().AddForce(moveDirection.normalized * Time.deltaTime * _pushbackIntensity);
+                        Debug.Log("Hit");
+                    }
                 }
+                else
+                {
+                    Debug.Log("Bruh");
+                }
+
             }
             else
             {
-                Debug.Log("Bruh");
+                shockWaveDuration = shockWaveDurationMax;
+                _isShockWavePossible = false;
+                _isShockWaveButtonPressed = false;
             }
         }
     }
