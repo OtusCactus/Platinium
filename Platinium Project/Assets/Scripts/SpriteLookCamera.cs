8c53a582e51e82b9728ffcdad1083c7734e7dca5
Otus, Cactus <matildemattr@gmail.com>
2019-10-18 16:04:14 +0200

    Commentaires

--- Platinium Project/Assets/Scripts/SpriteLookCamera.cs
+++ Platinium Project/Assets/Scripts/SpriteLookCamera.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class SpriteLookCamera : MonoBehaviour
 {
+    //Matilde a fait ce script
+    //Ce script permet de faire en sorte que les objets regardent la caméra
+    //On le garde dans l'optique de faire des sprites à la mode de "Don't Starve", où les sprites sont toujours face à la caméra
+
     // Start is called before the first frame update
     void Start()
     {
