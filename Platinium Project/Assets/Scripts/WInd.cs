66f300b3fa8c0b9c65fb1de0cb542274470d1f5d
Grégoire <gpierillas@gmail.com>
2019-11-14 18:50:16 +0100

    Wind feature + setup build sound designer

---
+++ Platinium Project/Assets/Scripts/WInd.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WInd : MonoBehaviour
+{
+
+    public GameObject gameManager;
+    private GameManager gameManagerScript;
+    public List<Rigidbody2D> playersRb;
+
+    public Vector2 direction;
+    public float windForce;
+
+    private void Awake()
+    {
+        gameManagerScript = gameManager.GetComponent<GameManager>();
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < gameManagerScript.playerList.Count; i++)
+        {
+            playersRb.Add(gameManagerScript.playerList[i].gameObject.GetComponent<Rigidbody2D>());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Rigidbody2D playersObjRb in playersRb)
+        {
+            playersObjRb.velocity -= direction * Time.deltaTime * windForce;
+        }
+    }
+}
