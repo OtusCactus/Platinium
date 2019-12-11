0abe64f08183c5e22d215a5be5883b7d68d5a47c
Otus, Cactus <matildemattr@gmail.com>
2019-12-11 09:42:39 +0100

    clouds

---
+++ Platinium Project/Assets/Scripts/Cloud.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cloud : MonoBehaviour
+{
+    public float speed = 2;
+    private RectTransform _myTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _myTransform = gameObject.GetComponent<Image>().rectTransform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _myTransform.anchoredPosition += Vector2.left * speed;
+    }
+}
